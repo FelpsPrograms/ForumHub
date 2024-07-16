8ca30ba83d21f3a7ab27e0b00635145fed613b52
Felipe Vasel <felipe.vasel30052023@gmail.com>
2024-07-16 10:08:39 -0300

    Geração token

--- src/main/java/com/felps/forum/controller/UsuarioController.java
+++ src/main/java/com/felps/forum/controller/UsuarioController.java
@@ -1,11 +1,15 @@
 package com.felps.forum.controller;
 
 import com.felps.forum.domain.usuario.DadosAutenticacao;
-import com.felps.forum.infra.security.SecurityConfigurations;
+import com.felps.forum.domain.usuario.Usuario;
+import com.felps.forum.infra.security.DadosTokenJwt;
+import com.felps.forum.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +22,17 @@ public class UsuarioController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
-        return ResponseEntity.ok().build();
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        Authentication authentication = manager.authenticate(authenticationToken);
+
+        String tokenJwt = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new DadosTokenJwt(tokenJwt));
     }
 
 }
---
+++ src/main/java/com/felps/forum/infra/security/DadosTokenJwt.java
@@ -0,0 +1,4 @@
+package com.felps.forum.infra.security;
+
+public record DadosTokenJwt(String token) {
+}
